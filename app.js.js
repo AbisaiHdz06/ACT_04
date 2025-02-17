58b09fa0186ca7f4a757db5bf807d7568b05d748
AbisaiHdz06 <Al03095697@tecmilenio.mx>
2025-02-17 08:48:35 -0600

    Add files via upload

---
+++ app.js.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+app.get("/api/status", (req, res) => {
+  res.json({ message: "server is running!" });
+});
+
+module.exports = app; // 👈 Importante: Exportar la app
+
